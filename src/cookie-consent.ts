4036b325cfad1b6c520840c612c99c896eb65c0a
Elisha Witte <elishawitte@gmail.com>
2019-08-06 15:03:51 +0200

    Add GTM events

--- src/cookie-consent.ts
+++ src/cookie-consent.ts
@@ -1,17 +1,30 @@
 import CookieNotification from './cookie-notification';
 import CookiePreferences from './cookie-preferences';
 import { CookieConsentOptions } from '../types';
+import CookieManager from './cookie-manager';
 
 export default class CookieConsent {
+  protected cookieManager: CookieManager;
   protected cookieNotification: CookieNotification;
   protected cookiePreferences: CookiePreferences;
   
   constructor(options: CookieConsentOptions = {}) {
+    this.cookieManager = new CookieManager(options);
     this.cookieNotification = new CookieNotification(options);
     this.cookiePreferences = new CookiePreferences(options);
   }
 
   init(): void {
+    // @ts-ignore
+    const gtm: any = dataLayer;
+    if (gtm !== undefined) {
+      if (this.cookieManager.hasMarketingCookie()) {
+        gtm.push({ event: 'cookie-consent.marketing' });
+      }
+      if (this.cookieManager.hasAnalyticsCookie()) {
+        gtm.push({ event: 'cookie-consent.analytics' });
+      }
+    }
     this.cookieNotification.init();
     this.cookiePreferences.init();
   }
