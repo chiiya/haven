737b5cc2879cb38ceedef8acaa2f95752efe3b6b
Elisha Witte <elishawitte@gmail.com>
2019-08-06 15:04:12 +0200

    Build files

--- dist/cookie-consent.d.ts
+++ dist/cookie-consent.d.ts
@@ -1,7 +1,9 @@
 import CookieNotification from './cookie-notification';
 import CookiePreferences from './cookie-preferences';
 import { CookieConsentOptions } from '../types';
+import CookieManager from './cookie-manager';
 export default class CookieConsent {
+    protected cookieManager: CookieManager;
     protected cookieNotification: CookieNotification;
     protected cookiePreferences: CookiePreferences;
     constructor(options?: CookieConsentOptions);
