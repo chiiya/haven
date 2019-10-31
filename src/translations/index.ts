1eaf5136dd01b403825179e15f9e1eb8b398fe16
Elisha Witte <elishawitte@gmail.com>
2019-10-31 19:39:52 +0100

    Add translations

---
+++ src/translations/index.ts
@@ -0,0 +1,10 @@
+import en from 'en.yml';
+import de from 'de.yml';
+import { Translations } from '../../types/translations';
+
+const translations: Translations = {
+  en: en,
+  de: de,
+};
+
+export default translations;
---
+++ types/translations.ts
@@ -0,0 +1,31 @@
+export interface Translations {
+  [lang: string]: Translation;
+}
+
+export interface Translation {
+  notification: NotificationTranslation;
+  preferences: PreferencesTranslation;
+  purposes: Purposes;
+}
+
+export interface NotificationTranslation {
+  message: string;
+  policy: string;
+  accept: string;
+  decline: string;
+}
+
+export interface PreferencesTranslation {
+  description: string;
+  save: string;
+  saved: string;
+}
+
+export interface Purposes {
+  [identifier: string]: Purpose;
+}
+
+export interface Purpose {
+  name: string;
+  description: string;
+}
