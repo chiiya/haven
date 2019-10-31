a960fb033fdba34c4e4e245645a66042a4d5f43c
Elisha Witte <elishawitte@gmail.com>
2019-10-31 19:50:03 +0100

    Improve default notification

--- src/notification/default-notification.ts
+++ src/notification/default-notification.ts
@@ -1,9 +1,96 @@
-import { HavenNotificationOptions } from '../../types';
-import { styles } from './default-styles';
 import store from '../store';
 import { trans } from '../utils';
 
-export const notification = (options: HavenNotificationOptions) => {
+const fontStack = [
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Segoe UI"',
+  'Roboto',
+  'Oxygen',
+  'Ubuntu',
+  'Cantarell',
+  '"Fira Sans"',
+  '"Droid Sans"',
+  '"Helvetica Neue"',
+  'Arial',
+  '"Noto Sans"',
+  'sans-serif',
+  '"Apple Color Emoji"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Noto Color Emoji"',
+];
+
+const createStyles = (): string => {
+  const options = store.notification.styles;
+
+  return `
+    .hv-notification {
+      align-items: center;
+      background: ${options.background};
+      box-sizing: border-box;
+      color: ${options.textColor};
+      display: flex;
+      font-family: ${fontStack.join(', ')};
+      font-size: 16px;
+      justify-content: space-between;
+      left: 0;
+      line-height: 1.5;
+      opacity: 1;
+      overflow: hidden;
+      padding: 16px 32px;
+      position: fixed;
+      right: 0;
+      transition: opacity 1s ease;
+      width: 100%;
+      z-index: 9999;
+    }
+
+    .hv-notification--top {
+      top: 0;
+    }
+
+    .hv-notification--bottom {
+      bottom: 0;
+    }
+
+    .hv-notification__message {
+      margin-right: 16px;
+    }
+
+    .hv-notification-button {
+      align-items: center;
+      border: 0;
+      cursor: pointer;
+      display: inline-flex;
+      font-size: 14px;
+      justify-content: center;
+      outline: 0;
+      padding: 8px 32px;
+      transition: background-color 0.2s ease, color 0.2s ease;
+    }
+
+    .hv-notification__decline {
+      background: transparent;
+    }
+
+    .hv-notification__decline:hover {
+      text-decoration: underline;
+    }
+
+    .hv-notification__accept {
+      background: ${options.buttonBackgroundColor};
+      color: ${options.buttonTextColor};
+    }
+
+    .hv-notification__accept:hover {
+      background: ${options.buttonBackgroundColorHover};
+    }
+  `;
+};
+
+const createNotification = (): string => {
+  const options = store.notification;
   return `
   <div id="cookie-notification" role="alert" class="hv-notification hv-notification--${options.position}" data-display="flex" style="display: none;">
     <p class="hv-notification__message">
@@ -18,13 +105,16 @@ export const notification = (options: HavenNotificationOptions) => {
 };
 
 export default class DefaultNotification {
+  /**
+   * Create and inject the default cookie notification.
+   */
   public static create() {
     const style = document.createElement('style');
     style.type = 'text/css';
-    style.appendChild(document.createTextNode(styles(store.notification.styles)));
+    style.appendChild(document.createTextNode(createStyles()));
     document.head.appendChild(style);
     const wrapper = document.createElement('div');
-    wrapper.innerHTML = notification(store.notification);
+    wrapper.innerHTML = createNotification();
     document.body.appendChild(wrapper);
   }
 }
