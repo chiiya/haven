a3c92accba69242a2a3168ef695e820ec579e577
Elisha Witte <elishawitte@gmail.com>
2019-11-04 16:24:05 +0100

    Fix custom purposes not being used

--- src/store/configuration-resolver.ts
+++ src/store/configuration-resolver.ts
@@ -22,7 +22,7 @@ export default class ConfigurationResolver {
    * @param options
    */
   public static resolveBaseConfiguration(options: Partial<HavenOptions>) {
-    for (const item of ['prefix', 'cookies', 'lang', 'type', 'services'])  {
+    for (const item of ['prefix', 'cookies', 'lang', 'type', 'services', 'purposes'])  {
       if (options[item] !== undefined) {
         store[item] = options[item];
       }
