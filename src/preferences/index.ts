315b213a0f7a5b910d4949d702143f8b6984a0bf
Elisha Witte <elishawitte@gmail.com>
2020-01-10 14:58:27 +0100

    Add (optional) save button for cookie preferences

--- src/preferences/index.ts
+++ src/preferences/index.ts
@@ -1,7 +1,7 @@
-import CookieManager from '../cookies/cookie-manager';
-import store from '../store';
-import DefaultPreferences from './default-preferences';
-import { getAllPurposes, trans } from '../utils';
+import CookieManager from "../cookies/cookie-manager";
+import store from "../store";
+import DefaultPreferences from "./default-preferences";
+import { getAllPurposes, trans } from "../utils";
 
 export default class CookiePreferences {
   /** Cookie manager instance */
@@ -20,7 +20,7 @@ export default class CookiePreferences {
    * Initialize cookie preferences initial states and event listeners.
    */
   public init(): void {
-    const wrapper = document.getElementById('cookie-preferences');
+    const wrapper = document.getElementById("cookie-preferences");
     if (wrapper !== null) {
       DefaultPreferences.create(wrapper);
     }
@@ -32,11 +32,15 @@ export default class CookiePreferences {
    */
   public attachListeners(): void {
     const purposes = getAllPurposes();
+    const checkboxes: { [purpose: string]: HTMLInputElement } = {};
     for (const purpose of purposes) {
-      const checkbox = <HTMLInputElement | null>document.getElementById(`cookie-preferences--${purpose}`);
+      const checkbox = <HTMLInputElement | null>(
+        document.getElementById(`cookie-preferences--${purpose}`)
+      );
       if (checkbox !== null) {
+        checkboxes[purpose] = checkbox;
         checkbox.checked = this.cookieManager.hasCookiesEnabled(purpose);
-        checkbox.addEventListener('change', () => {
+        checkbox.addEventListener("change", () => {
           this.cookieManager.enableFunctionalCookie();
           if (checkbox.checked) {
             this.cookieManager.enableCookies(purpose);
@@ -45,6 +49,24 @@ export default class CookiePreferences {
           }
         });
       }
+    }
+
+    const saveButton = document.getElementById("cookie-preferences__save");
+    if (saveButton !== null) {
+      saveButton.addEventListener("click", () => {
+        this.cookieManager.enableFunctionalCookie();
+        for (const purpose of purposes) {
+          if (checkboxes[purpose].checked) {
+            this.cookieManager.enableCookies(purpose);
+          } else {
+            this.cookieManager.disableCookies(purpose);
+          }
+        }
+        const notification = document.getElementById("cookie-notification");
+        if (notification !== null) {
+          notification.style.display = "none";
+        }
+      });
     }
   }
 }
