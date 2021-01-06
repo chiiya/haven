4b37a57d244a5c24dc466d47815c6a9c2c4189f6
Elisha Witte <elishawitte@gmail.com>
2021-01-06 22:00:35 +0100

    Fix SSR compatibility?

--- src/index.umd.ts
+++ src/index.umd.ts
@@ -1,6 +1,8 @@
 import CookieManager from './cookies/cookie-manager';
 import Haven from './haven';
 
-window.Haven = Haven;
+if (typeof window !== 'undefined') {
+  window.Haven = Haven;
+}
 
 export { CookieManager, Haven };
