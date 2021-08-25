e80b79ec071759bb8fecebced36ac3d17647efec
Mejans <61360811+Mejans@users.noreply.github.com>
2021-08-25 21:22:25 +0200

    adds OC to the list

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,5 +1,6 @@
 import en from './en.yml';
 import de from './de.yml';
+import oc from './oc.yml';
 import { Translations } from '../types/translations';
 
 const translations: Translations = {
@@ -7,6 +8,8 @@ const translations: Translations = {
   en: en,
   // @ts-ignore
   de: de,
+  // @ts-ignore
+  oc: oc,  
 };
 // @ts-ignore-end
 
