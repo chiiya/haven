6e1f02a90c11ae79c4e8d956b422f8a4b313f054
Elisha Witte <elishawitte@gmail.com>
2021-09-29 10:32:35 +0200

    Add build config

---
+++ build/plugin-config.js
@@ -0,0 +1,59 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+const rollupConfig = (name = 'index') => {
+  return [
+    {
+      input: 'src/index.ts',
+      output: {
+        dir: 'dist/esm',
+        format: 'esm',
+      },
+      plugins: [
+        babel({
+          extensions,
+          include: ['src/**/*'],
+          babelHelpers: 'runtime',
+          plugins: [
+            '@babel/proposal-class-properties',
+            ['@babel/plugin-transform-runtime', { version: '^7.11.2' }],
+          ],
+        }),
+        nodeResolve({
+          extensions,
+        }),
+      ],
+      preserveModules: true,
+      external: /@anshin|(@babel\/runtime)/,
+    },
+    {
+      input: 'src/index.umd.ts',
+      output: {
+        file: `dist/${name}.js`,
+        format: 'umd',
+      },
+      plugins: [
+        babel({
+          extensions,
+          include: ['src/**/*'],
+          babelHelpers: 'inline',
+          plugins: ['@babel/proposal-class-properties'],
+        }),
+        replace({
+          'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        nodeResolve({
+          extensions,
+        }),
+        commonjs(),
+        terser(),
+      ],
+    },
+  ];
+};
+
+module.exports = rollupConfig;
